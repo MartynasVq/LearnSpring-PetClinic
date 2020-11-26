5298e7c32878641f89e38e2d3e21a2d480e0b88b
Martynas <nbtsst@gmail.com>
2020-11-26 21:44:53 +0200

    Initial commit

---
+++ src/main/java/com/learn/learnspringpetclinic/LearnspringPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.learn.learnspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnspringPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/learnspringpetclinic/LearnspringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.learnspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
