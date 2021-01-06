50ba75fd503358300a4112e92182c39a2fbe619c
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-06 20:42:00 +0200

    Added specialty service

---
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.learn.learnspringpetclinic.services;
+
+import com.learn.learnspringpetclinic.model.Specialty;
+
+import java.util.Set;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+
+}
