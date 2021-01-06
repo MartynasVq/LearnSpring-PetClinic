1165f1c2bc57784449b904a0800de4c9f54e8da2
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-06 19:18:30 +0200

    Add contact info to owner

--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Owner.java
@@ -6,6 +6,9 @@ public class Owner extends Person {
 
 
     private Set<Pet> pets;
+    private String address;
+    private String city;
+    private String phone;
 
     public Owner() {
 
@@ -18,5 +21,29 @@ public class Owner extends Person {
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
