7a610e80f930673ab529bf4edbdc15732b50f66d
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-06 19:15:00 +0200

    Added vet specialties

--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Vet.java
@@ -1,10 +1,20 @@
 package com.learn.learnspringpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
 
-
+    private Set<Specialty> specialties;
 
     public Vet() {
 
+    }
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
     }
 }
