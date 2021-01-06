cbd7fbebd0ae4d03245a597869e12a96786a683b
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-06 19:26:32 +0200

    Implement pet type service

---
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.learn.learnspringpetclinic.services;
+
+import com.learn.learnspringpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+
+
+
+}
