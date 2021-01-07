6c32a21e5cd5ebbd3cd513f7ffc506dae6398b82
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-07 20:46:12 +0200

    Owner entity JPA mappings

--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Person.java
@@ -1,8 +1,14 @@
 package com.learn.learnspringpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity {
 
+    @Column(name = "first_name")
     private String name;
+    @Column(name = "last_name")
     private String lastName;
 
 
--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Pet.java
@@ -1,12 +1,21 @@
 package com.learn.learnspringpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
 
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birthday")
     private LocalDate birthday;
+    @Column(name = "name")
     private String name;
 
     public String getName() {
