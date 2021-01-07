60bb9a06924015b9bcec687fb91d6b56d566550a
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-07 20:34:18 +0200

    JPA mapping for base entity

--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/BaseEntity.java
@@ -1,9 +1,18 @@
 package com.learn.learnspringpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+
+//inform that this class is superclass and does not need to be in the db table separately
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
