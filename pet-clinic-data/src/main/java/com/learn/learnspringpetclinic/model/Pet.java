4859bc5dfb318a15330a3d7abf31545f0f8800cd
Martynas <58885736+MartynasVq@users.noreply.github.com>
2021-01-27 13:56:52 +0200

    Create/update pet form

--- pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/learn/learnspringpetclinic/model/Pet.java
@@ -1,10 +1,13 @@
 package com.learn.learnspringpetclinic.model;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,7 +15,6 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -24,10 +26,58 @@ public class Pet extends BaseEntity {
     @JoinColumn(name = "owner_id")
     private Owner owner;
     @Column(name = "birthday")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
     @Column(name = "name")
     private String name;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visitSet = new HashSet<>();
 
+    public static PetBuilder builder() {
+        return new PetBuilder();
+    }
+
+    public static class PetBuilder {
+        private PetType petType;
+        private Owner owner;
+        private LocalDate birthday;
+        private String name;
+        private Set<Visit> visitSet;
+
+        PetBuilder() {
+        }
+
+        public PetBuilder petType(PetType petType) {
+            this.petType = petType;
+            return this;
+        }
+
+        public PetBuilder owner(Owner owner) {
+            this.owner = owner;
+            return this;
+        }
+
+        public PetBuilder birthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public PetBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PetBuilder visitSet(Set<Visit> visitSet) {
+            this.visitSet = visitSet;
+            return this;
+        }
+
+        public Pet build() {
+            return new Pet(petType, owner, birthday, name, visitSet);
+        }
+
+        public String toString() {
+            return "Pet.PetBuilder(petType=" + this.petType + ", owner=" + this.owner + ", birthday=" + this.birthday + ", name=" + this.name + ", visitSet=" + this.visitSet + ")";
+        }
+    }
 }
